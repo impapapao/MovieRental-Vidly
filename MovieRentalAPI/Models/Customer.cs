c4503f7a826417a8e709afa83af31fd5a9416b61
impaolo15 <john.paolo1724@gmail.com>
2023-03-28 15:38:08 +0800

    Added entity framework

---
+++ MovieRentalAPI/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieRentalAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Province { get; set; } = string.Empty;
+    }
+}
