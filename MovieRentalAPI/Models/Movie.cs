9dc96ec1b90c1741d44118b18fb7ea39610629b5
impaolo15 <john.paolo1724@gmail.com>
2023-03-28 15:27:14 +0800

    Add project files.

---
+++ MovieRentalAPI/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieRentalAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieDescription { get; set;}
+        public string Genre { get; set;}
+       
+    }
+}
