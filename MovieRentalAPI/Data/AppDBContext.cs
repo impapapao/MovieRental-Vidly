757a6e9ce0a3d2cca2cf56f0b09ee9e554b65276
impaolo15 <john.paolo1724@gmail.com>
2023-03-28 16:52:55 +0800

    Added DataBase and Added tables in Database

---
+++ MovieRentalAPI/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRentalAPI.Models;
+
+namespace MovieRentalAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
--- MovieRentalAPI/Startup.cs
+++ MovieRentalAPI/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MovieRentalAPI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +18,11 @@ namespace MovieRentalAPI
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("MoviesConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -28,6 +32,9 @@ namespace MovieRentalAPI
         {
 
             services.AddControllers();
+
+            //Configure DBContext wit SQL
+            services.AddDbContext<AppDBContext>(options => options.UseSqlServer(ConnectionString));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MovieRentalAPI", Version = "v1" });
